eba36d14ca19c3956e7b0acec43ee712516face8
Jeronimo19 <127432939+Jeronimo19@users.noreply.github.com>
2024-03-07 10:49:37 -0600

    Add files via upload

---
+++ conexionbd.php
@@ -0,0 +1,8 @@
+﻿<?php
+//este codigo realiza la declaración de variables
+//que serán utilizadas en el index
+$host = 'localhost'; //Estoy declarando variables
+$username = 'root'; //Declaracion del nombre de usuario dela b.database
+$password = '';//contraseña de la b.d vacia
+$database = 'cajademensajes'; //nombre de la base de datos
+?>
